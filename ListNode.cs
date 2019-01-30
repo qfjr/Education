1aec680cdcef48badde10dc68690a852ccbe8808
qfjr <qfulgham@outlook.com>
2019-01-30 12:14:41 -0500

    Add files via upload

---
+++ ListNode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Linked_List_Lesson
+{
+    class ListNode<T>
+    {
+        public T data;
+        public ListNode<T> next;
+
+        public static ListNode<int> search(ListNode<int> L, int key)
+        {
+            while(L != null && L.data != key)
+            {
+                L = L.next;
+            }
+
+            // If key was not present in the list, L will have become null
+            return L;
+        }
+
+
+    }
+}
