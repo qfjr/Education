a9134a244362dff3eef9f968c5232a3bfb273484
qfjr <qfulgham@outlook.com>
2019-01-30 12:10:24 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace continuation_task_lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int> firstTask = Task.Factory.StartNew<int>(() =>
+            {
+                Console.WriteLine("First Task");
+                return 42;
+            });
+
+            Task secondTask = firstTask.ContinueWith((ft) => {
+                Console.WriteLine("Second Task, First task returned {0}", ft.Result);
+            });
+        }
+    }
+}
