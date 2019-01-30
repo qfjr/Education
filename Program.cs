f11a8c7dd7a9d1a5036d4f0e7e7402134bf93118
qfjr <qfulgham@outlook.com>
2019-01-30 12:11:33 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lambda_lesson
+{
+    class Program
+    {
+        delegate int CountIt(int x);
+
+        static void Main(string[] args)
+        {
+            // Example Code of Expression Lambda
+
+            CountIt testDel = (int x) => x + 5;
+
+            int result = testDel(5);
+        }
+    }
+}
