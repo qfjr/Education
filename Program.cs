8a707a0bc7f2f0e4870069ee3f584660a8831a23
qfjr <qfulgham@outlook.com>
2019-01-30 12:00:18 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dictionary_lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> superheroes = new Dictionary<string, string>();
+
+            superheroes.Add("Clark Kent", "Superman");
+            superheroes.Add("Bruce Wanye", "Batman");
+            superheroes.Add("Barry West", "Flash");
+
+            superheroes.Remove("Bruce Wayne");
+
+            superheroes.TryGetValue("Clark Kent", out string value);
+
+            Console.WriteLine(value);
+        }
+    }
+}
