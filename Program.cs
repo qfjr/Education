886bb237d1f73844e82ff77481bcb090cfce5a2e
qfjr <qfulgham@outlook.com>
2019-01-30 12:06:32 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace thread_lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(Thread1);
+            t.Start();
+
+            for (var n=0; n < 10; n++)
+            {
+                Console.WriteLine(n);
+            }
+
+            Console.ReadKey();
+            
+        }
+
+        static void Thread1()
+        {
+            for (var n = 10; n < 20; n++)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
