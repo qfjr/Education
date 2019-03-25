5521d96c142fb066674ed27d76a11f44447bbb61
qfjr <qfulgham@outlook.com>
2019-03-25 08:56:45 -0400

    Update simple_array_sum.cs

--- HackerRank/Algortihms/Easy Problem Solving/simple_array_sum.cs
+++ HackerRank/Algortihms/Easy Problem Solving/simple_array_sum.cs
@@ -1,2 +1,21 @@
 // Simple Array Sum
-// Given an array of integers, find the sum of its elements. For example
+// Given an array of integers, find the sum of its elements. For example 
+// if the array ar = [1,2,3], then 1 + 2 + 3 = 6, so return 6
+// Sample Input: 6(elements), array(elements)1,2,3,4,10,11
+// Sample Output:31
+
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Solution
+{
+  /* Complete the simapleArraySum function below */
+  static int simpleArraySum(int[] ar)
+  {
+    /* write your code here */
+    
+    
+  }
